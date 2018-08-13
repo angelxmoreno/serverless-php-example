3e49f94a59e2832b9567398d2ee2aeffdd714f2f
Angel S. Moreno <angelxmoreno@gmail.com>
2018-08-13 19:47:53 -0400

    reversing route order

--- bref.php
+++ bref.php
@@ -5,15 +5,17 @@ use Bref\Bridge\Slim\SlimAdapter;
 require __DIR__.'/vendor/autoload.php';
 
 $slim = new Slim\App;
+
+
+$slim->get('/dev/login', function ($request, $response) {
+    $response->getBody()->write('wanna login?');
+    return $response;
+});
 $slim->get('/dev', function ($request, $response) {
     $response->getBody()->write('Hello world!');
     return $response;
 });
 
-$slim->get('/login', function ($request, $response) {
-    $response->getBody()->write('wanna login?');
-    return $response;
-});
 
 $app = new \Bref\Application;
 $app->httpHandler(new SlimAdapter($slim));
