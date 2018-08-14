e6ac59232d0159a31cf8b5e39a51d230783da25b
Angel S. Moreno <angelxmoreno@gmail.com>
2018-08-13 20:24:07 -0400

    Added actual functions

--- bref.php
+++ bref.php
@@ -1,22 +1,38 @@
 <?php
 
+use Bref\Application as BrefApplication;
 use Bref\Bridge\Slim\SlimAdapter;
+use Slim\App as SlimApp;
+use Slim\Http\Response;
+use Zend\Diactoros\ServerRequest as Request;
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+$slim = new SlimApp;
+$slim->post('/dev/f2c', function (Request $request, Response $response) {
+    $params = $request->getParsedBody();
+    $f = (int)$params['f'];
+    $c = ($f - 32) * .5556;
 
-$slim = new Slim\App;
-
-
-$slim->get('/dev/login', function ($request, $response) {
-    $response->getBody()->write('wanna login?');
-    return $response;
+    return $response->withJson([
+        'fahrenheit' => $f,
+        'celsius' => $c,
+    ]);
 });
-$slim->get('/dev', function ($request, $response) {
+$slim->post('/dev/c2f', function (Request $request, Response $response) {
+    $params = $request->getParsedBody();
+    $c = (int)$params['c'];
+    $f = ($c * 9 / 5) + 32;
+
+    return $response->withJson([
+        'fahrenheit' => $f,
+        'celsius' => $c,
+    ]);
+});
+$slim->get('/dev', function (Request $request, Response $response) {
     $response->getBody()->write('Hello world!');
+
     return $response;
 });
-
-
-$app = new \Bref\Application;
+$app = new BrefApplication;
 $app->httpHandler(new SlimAdapter($slim));
 $app->run();
